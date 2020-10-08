cb832375c968af15fc2752ace8009387f0f1da4e
lipowada <lipowada@fel.cvut.cz>
2020-10-08 23:54:54 +0200

    fix mobile view

--- pages/index.js
+++ pages/index.js
@@ -127,23 +127,6 @@ class MobileContainer extends Component {
     return (
         <Media as={Sidebar.Pushable} at='mobile'>
           <Sidebar.Pushable>
-            <Sidebar
-                as={Menu}
-                animation='overlay'
-                inverted
-                onHide={this.handleSidebarHide}
-                vertical
-                visible={sidebarOpened}
-            >
-              <Menu.Item as='a' active>
-                Home
-              </Menu.Item>
-              <Menu.Item as='a'>Work</Menu.Item>
-              <Menu.Item as='a'>Company</Menu.Item>
-              <Menu.Item as='a'>Careers</Menu.Item>
-              <Menu.Item as='a'>Log in</Menu.Item>
-              <Menu.Item as='a'>Sign Up</Menu.Item>
-            </Sidebar>
 
             <Sidebar.Pusher dimmed={sidebarOpened}>
               <Segment
@@ -153,19 +136,6 @@ class MobileContainer extends Component {
                   vertical
               >
                 <Container>
-                  <Menu inverted pointing secondary size='large'>
-                    <Menu.Item onClick={this.handleToggle}>
-                      <Icon name='sidebar' />
-                    </Menu.Item>
-                    <Menu.Item position='right'>
-                      <Button as='a' inverted>
-                        Log in
-                      </Button>
-                      <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                        Sign Up
-                      </Button>
-                    </Menu.Item>
-                  </Menu>
                 </Container>
                 <HomepageHeading mobile />
               </Segment>
